ee6378ad0fcdea20b422f00604ce38b87f8c0f9c
coranos <coranos@users.noreply.github.com>
2023-02-12 12:17:50 -0600

    updating dependencies

--- scripts/index.js
+++ scripts/index.js
@@ -94,7 +94,7 @@ const receivePending = async (representative, seed, channel) => {
   }
 };
 
-const init = async () => {
+const init = () => {
   process.on('SIGINT', closeProgram);
   overrideConfig();
   bananojs.setBananodeApiUrl(config.bananodeApiUrl);
@@ -102,22 +102,22 @@ const init = async () => {
   const walletAccount = bananojs.getBananoAccountFromSeed(config.walletSeed, SEED_IX);
 
   discordClient.on('messageReactionAdd', async (reaction, user) => {
-  	// When a reaction is received, check if the structure is partial
-  	if (reaction.partial) {
-  		// If the message this reaction belongs to was removed, the fetching might result in an API error which should be handled
-  		try {
-  			await reaction.fetch();
-  		} catch (error) {
-  			console.error('Something went wrong when fetching the message:', error);
-  			// Return as `reaction.message.author` may be undefined/null
-  			return;
-  		}
-  	}
+    // When a reaction is received, check if the structure is partial
+    if (reaction.partial) {
+      // If the message this reaction belongs to was removed, the fetching might result in an API error which should be handled
+      try {
+        await reaction.fetch();
+      } catch (error) {
+        console.error('Something went wrong when fetching the message:', error);
+        // Return as `reaction.message.author` may be undefined/null
+        return;
+      }
+    }
 
-  	// Now the message has been cached and is fully available
-  	console.log(`${reaction.message.author}'s message "${reaction.message.content}" gained a reaction!`);
-  	// The reaction is now also fully available and the properties will be reflected accurately:
-  	console.log(`${reaction.count} user(s) have given the same reaction to this message!`);
+    // Now the message has been cached and is fully available
+    console.log(`${reaction.message.author}'s message "${reaction.message.content}" gained a reaction!`);
+    // The reaction is now also fully available and the properties will be reflected accurately:
+    console.log(`${reaction.count} user(s) have given the same reaction to this message!`);
 
     const guild = discordClient.guilds.cache.get(reaction.message.guild.id);
     const member = await guild.members.fetch(user.id);
@@ -126,22 +126,22 @@ const init = async () => {
   });
 
   discordClient.on('messageReactionRemove', async (reaction, user) => {
-  	// When a reaction is received, check if the structure is partial
-  	if (reaction.partial) {
-  		// If the message this reaction belongs to was removed, the fetching might result in an API error which should be handled
-  		try {
-  			await reaction.fetch();
-  		} catch (error) {
-  			console.error('Something went wrong when fetching the message:', error);
-  			// Return as `reaction.message.author` may be undefined/null
-  			return;
-  		}
-  	}
+    // When a reaction is received, check if the structure is partial
+    if (reaction.partial) {
+      // If the message this reaction belongs to was removed, the fetching might result in an API error which should be handled
+      try {
+        await reaction.fetch();
+      } catch (error) {
+        console.error('Something went wrong when fetching the message:', error);
+        // Return as `reaction.message.author` may be undefined/null
+        return;
+      }
+    }
 
-  	// Now the message has been cached and is fully available
-  	console.log(`${reaction.message.author}'s message "${reaction.message.content}" lost a reaction!`);
-  	// The reaction is now also fully available and the properties will be reflected accurately:
-  	console.log(`${reaction.count} user(s) have given the same reaction to this message!`);
+    // Now the message has been cached and is fully available
+    console.log(`${reaction.message.author}'s message "${reaction.message.content}" lost a reaction!`);
+    // The reaction is now also fully available and the properties will be reflected accurately:
+    console.log(`${reaction.count} user(s) have given the same reaction to this message!`);
 
     const guild = discordClient.guilds.cache.get(reaction.message.guild.id);
     const member = await guild.members.fetch(user.id);
